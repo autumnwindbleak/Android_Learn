813349110c47be9bc6d498758b25b7fb4dd90bf1
Ian <autumnwindbleak@gmail.com>
2018-12-10 10:02:56 +1100

    Initial commit

---
+++ app/src/main/java/com/example/ian/detect/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.ian.detect;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static String MESSAGE = "com.example.ian.detect.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * call when user do click on the button
+     * @param view
+     */
+    public void sendMessage(View view){
+        Intent intent = new Intent(this,DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(MESSAGE,message);
+        startActivity(intent);
+    }
+
+
+}
