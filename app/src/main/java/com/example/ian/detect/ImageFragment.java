0db98f0a1c69eb589bba59e9ffcb563c6291f51e
Ian <autumnwindbleak@gmail.com>
2019-01-04 14:48:51 +1100

    add Gaussian blur function
    add Gaussian blur funtion to the imageFragment, tried edit image by ourself but too slow.

--- app/src/main/java/com/example/ian/detect/ImageFragment.java
+++ app/src/main/java/com/example/ian/detect/ImageFragment.java
@@ -5,12 +5,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
 import android.net.Uri;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -69,6 +77,10 @@ public class ImageFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
+
+        /**
+         * recieve the image and then show it on image view
+         */
         LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(getActivity());
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction("VideoFragment");
@@ -76,10 +88,46 @@ public class ImageFragment extends Fragment {
             @Override
             public void onReceive(Context context, Intent intent) {
                 Bitmap bitmap = intent.getParcelableExtra("image");
+                if(SelfConfiguration.GAUSSIAN_BLUR){
+                    bitmap = GaussianBlur(getContext(),bitmap,25,5);
+                }
                 mImageView.setImageBitmap(bitmap);
                 mImageView.setImageAlpha(255);
             }
         };
         localBroadcastManager.registerReceiver(broadcastReceiver,intentFilter);
+    }
+
+    /**
+     * Guassian Blur of a picture
+     * @param context
+     * @param source bitmap source
+     * @param radius blur radius (0-25)
+     * @param scaleratio smaller the image then go blur
+     * @return
+     */
+    private Bitmap GaussianBlur(Context context,Bitmap source, int radius,int scaleratio){
+        Bitmap inputbitmap = Bitmap.createScaledBitmap(source,source.getWidth()/scaleratio,source.getHeight()/scaleratio,false);
+        //create a renderscript
+        RenderScript renderScript = RenderScript.create(context);
+        //allocate memory for renderscript
+        final Allocation input = Allocation.createFromBitmap(renderScript,inputbitmap);
+        final Allocation output = Allocation.createTyped(renderScript,input.getType());
+        //load blur script
+        ScriptIntrinsicBlur scriptIntrinsicBlur = ScriptIntrinsicBlur.create(renderScript, Element.U8_4(renderScript));
+        //set input and parameters
+        scriptIntrinsicBlur.setInput(input);
+        scriptIntrinsicBlur.setRadius(radius);
+        //start the script
+        scriptIntrinsicBlur.forEach(output);
+        //copy result
+        output.copyTo(inputbitmap);
+        //destroy the renderscript
+        input.destroy();
+        output.destroy();
+        renderScript.destroy();
+        scriptIntrinsicBlur.destroy();
+        inputbitmap = Bitmap.createScaledBitmap(inputbitmap,source.getWidth(),source.getHeight(),false);
+        return inputbitmap;
     }
 }
--- app/src/main/java/com/example/ian/detect/SelfConfiguration.java
+++ app/src/main/java/com/example/ian/detect/SelfConfiguration.java
@@ -1,6 +1,7 @@
 package com.example.ian.detect;
 
 public class SelfConfiguration {
-    public static final boolean LOOP_TAKE_IMAGE = true;
+    public static final boolean LOOP_TAKE_IMAGE = false;
     public static final boolean SEND_IMAGE_BROADCAST = true;
+    public static final boolean GAUSSIAN_BLUR = false;
 }
